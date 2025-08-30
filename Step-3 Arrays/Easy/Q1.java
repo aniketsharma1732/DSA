fa47e46af32d09cd0799d0b8ce82db38b9678954
Aniket Sharma <anikets.5061@gmail.com>
2025-08-30 20:32:23 +0530

    array  commit

--- Step-3 Arrays/Easy/Q1.java
+++ Step-3 Arrays/Easy/Q1.java
@@ -1,12 +1,7 @@
 public class Q1{
 
     public static void main(String[] args) {
-        int a[]={1,2,3,4,2,22,4};
-        int n=a.length;
-        int largestElement=Integer.MIN_VALUE;
-        for(int i=0; i<n; i++){
-            
-        }
+        
     }
 
 
